bef844433426b20f8a6dfc39bdf42cb81c16e4c3
Samuel Mejía <sf977996@gmail.com>
2024-05-14 01:05:51 -0600

    sa2

--- src/main/java/sv/edu/udb/sistemacasospa/conexion.java
+++ src/main/java/sv/edu/udb/sistemacasospa/conexion.java
@@ -5,20 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexion {
-    private static Connection connection;
+    private static Connection con;
 
     public static Connection getConnection() {
-        if (connection == null) {
+        if (con == null) {
             try {
-                String url = "jdbc:mysql://localhost:3306/sistema_caso";
-                String user = "root";
-                String password = "tuContraseña";
+                // Cambiado el nombre del driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
 
-                connection = DriverManager.getConnection(url, user, password);
+                String url = "jdbc:mysql://localhost:3306/sistema_casos";
+                String user = "root";
+                String password = "";
+
+                con = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
-        return connection;
+        return con;
     }
 }
